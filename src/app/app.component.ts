bbd74272b6d7c1357cf679e3d5df657925f5016b
KaseyDev85 <w0841108@myscc.ca>
2024-09-18 19:04:55 -0400

    Assignment 2 Finished
    -> added 6 instances
    -> used two structural directives
     - NgFor
     - NgIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Vehicles } from './models/Vehicles';
+import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Kasey_Johns_Learning_Angular';
-  nothing = "Nothing";
-  something = "Something";
+  vehicles: Vehicles[] =
+  [
+    {id:1, year:"2000", make:"GMC", model:"Sierra", price:20, owned: false},
+    {id:2, year:"2003", make:"Mazda", model:"Rx-8", price:15000, owned: false},
+    {id:3, year:"2005", make:"Honda", model:"Accord", owned: true},
+    {id:4, year:"2010", make:"Kia", model:"Soul", owned: true},
+    {id:5, year:"2012", make:"Ford", model:"F-150", price:300000, owned: false},
+    {id:6, year:"2023", make:"Nissan", model:"GTR", owned: true},
+
+  ] 
+
+ 
+
 }
 
---
+++ src/app/models/Vehicles.ts
@@ -0,0 +1,8 @@
+export interface Vehicles {
+    id: number;
+    year: string;
+    make: string;
+    model: string;
+    price?: number;
+    owned?: boolean
+}
