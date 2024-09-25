53bfb590d848ab678944e8aebe9cd76642e55d83
KaseyDev85 <w0841108@myscc.ca>
2024-09-25 15:26:00 -0400

    Made two new components
    -> Made Vehicle-list component
    -> Made Vehicles-List-Items component

---
+++ src/app/vehicle-list-item/vehicle-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicle-list-item.component.html',
+  styleUrl: './vehicle-list-item.component.scss'
+})
+export class VehicleListItemComponent {
+
+}
