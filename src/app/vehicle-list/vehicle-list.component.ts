306883da746aa6967726d1e52ec2d9a2b1e5c774
KaseyDev85 <w0841108@myscc.ca>
2024-09-25 20:03:01 -0400

    Added background Colors
    -> made the 1 & 3 vehicles have a background colour of cyan
    -> made the 2 & 4 vehicles have background colour of red
    -> All inside a for loop

--- src/app/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle-list/vehicle-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Vehicles } from '../models/Vehicles';
 import { VehicleListItemComponent } from '../vehicle-list-item/vehicle-list-item.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-vehicle-list',
   standalone: true,
-  imports: [VehicleListItemComponent],
+  imports: [VehicleListItemComponent, NgFor, NgIf],
   templateUrl: './vehicle-list.component.html',
   styleUrl: './vehicle-list.component.scss'
 })
@@ -14,8 +15,8 @@ export class VehicleListComponent {
   [
     {id:1, year:"2000", make:"Toyota", model:"EA86", price:20, owned: false},
     {id:2, year:"2003", make:"Mazda", model:"Cx-7", price:15000, owned: false},
-    {id:3, year:"2005", make:"Honda", model:"Civic", owned: true},
-    {id:4, year:"2010", make:"Ford", model:"F-150", owned: true},
+    {id:3, year:"2005", make:"Honda", model:"Civic", price: 300, owned: true},
+    {id:4, year:"2010", make:"Ford", model:"F-150", price: 539032, owned: true},
     
 
   ] 
