5c60fa04b033889613008a434b6e00b3f08815cf
KaseyDev85 <w0841108@myscc.ca>
2024-10-02 17:28:27 -0400

    Made new files
    * Made mock-content.ts
    * Made service with the name Vehicles

--- src/app/data/mock-content.ts
+++ src/app/data/mock-content.ts
@@ -10,7 +10,15 @@ import { NgFor, NgIf } from '@angular/common';
   templateUrl: './vehicle-list.component.html',
   styleUrl: './vehicle-list.component.scss'
 })
-export class VehicleListComponent {
-  
+export class mockComponent {
+  vehicles: Vehicles[] =
+  [
+    {id:1, year:"2000", make:"Toyota", model:"EA86", price:20, owned: false},
+    {id:2, year:"2003", make:"Mazda", model:"Cx-7", price:15000, owned: false},
+    {id:3, year:"2005", make:"Honda", model:"Civic", price: 300, owned: true},
+    {id:4, year:"2010", make:"Ford", model:"F-150", price: 539032, owned: true},
+    
+
+  ] 
 
 }
