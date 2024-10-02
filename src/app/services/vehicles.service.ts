2ae04217211d018d9f74751d70e4eb55770a204e
KaseyDev85 <w0841108@myscc.ca>
2024-10-02 19:07:08 -0400

    Added the CRUD into the VehicleService
    * There is a get one Vehicle item
    * There is a add Vehicle
    * There is a update Vehicle
    * There is a Remove Vehicle

--- src/app/services/vehicles.service.ts
+++ src/app/services/vehicles.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { vehicles } from '../data/mock-content';
 import { Observable, of } from 'rxjs';
 import { Vehicles } from '../models/Vehicles';
+import { Input } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,38 @@ export class VehiclesService {
     return of(vehicles);
   }
 
+  getVehicleItem(num: number): Observable<Vehicles[]>{
+    
+    vehicles.forEach(vehicle => {
+      if(vehicle.id == num){
+        return of(vehicle);
+      }
+    });
+    return undefined;
+  }
+
+  @Input() AddVehicle(newItem: Vehicles): Observable<Vehicles[]> {
+    vehicles.push(newItem);
+    return of(vehicles);
+  } 
+
+  @Input() UpdateVehicle(id: number, item: Vehicles): Observable<Vehicles[]> {
+    vehicles.forEach(vehicle => {
+      if(vehicle.id == id){
+        vehicles.splice(vehicles.indexOf(vehicle), 1);
+        vehicles.push(item);
+      }
+    })
+    return of(vehicles);
+  }
+  @Input() RemoveVehicle(id: number): Observable<Vehicles[]> {
+    let item;
+    vehicles.forEach(vehicle => {
+      if(vehicle.id == id){
+        item = vehicles.splice(vehicles.indexOf(vehicle), 1);
+      }
+    })
+    return of(item);
+  }
 
 }
