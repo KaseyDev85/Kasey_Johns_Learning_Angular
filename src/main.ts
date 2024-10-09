6ef395255d499b3c346864785ea96ae24267c3f4
KaseyDev85 <w0841108@myscc.ca>
2024-10-09 18:29:35 -0400

    Finished steps 3 - 5
    * added the router into main.ts
    * added a list of routes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { vehicles } from './app/data/mock-content';
+import { VehicleListComponent } from './app/vehicle-list/vehicle-list.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  { path: 'vehicle', component: VehicleListComponent },
+  { path: '', redirectTo: '/Vehicles', pathMatch: 'full'}
+];
+
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
+
